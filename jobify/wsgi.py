178ba84c8515885db0d57c9ecb972a34c976e46a
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2026-03-21 19:02:34 +0300

    Fix Django configuration: environ setup, package renaming to lowercase

--- jobify/wsgi.py
+++ jobify/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Jobify project.
+WSGI config for jobify project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Jobify.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobify.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Jobify.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobify.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
