727802c02c430ccfd2a10368180323d2ef603692
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2026-04-07 20:11:37 +0300

    docs: add TODO models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+# TODO: Добавить модель User с расширенным профилем
+# TODO: Добавить модель City для географии вакансий
+# TODO: Добавить модель Category для классификации
