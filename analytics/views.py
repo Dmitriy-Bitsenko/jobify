b92d4f3446a36e6c15c2671671abc22f622bd6df
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2026-04-08 21:35:33 +0300

    docs: TODO for DashboardView

--- analytics/views.py
+++ analytics/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+ # TODO: DashboardView — дашборд с аналитикой
+# - График: ТОП-10 навыков по зарплатам
+# - График: Вакансии по городам
+# - Фильтры: город, зарплата, дата
+# - Использовать Chart.js для визуализации
